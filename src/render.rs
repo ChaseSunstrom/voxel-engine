08cc2c6a73af1f350273f937956c1f67fb2cde94
Chase Sunstrom <122314695+ChaseSunstrom@users.noreply.github.com>
2023-06-07 12:00:29 -0500

    Changed the color of the cubes.

--- src/render.rs
+++ src/render.rs
@@ -1,8 +1,19 @@
+use bevy::pbr::MaterialProperties;
 use bevy::prelude::*;
+use bevy::render::render_resource::ShaderRef::Handle;
+
 
 //no clue what this does
-pub fn render_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
-    let mesh = mesh_assets.add(shape::Box::new(1.0, 1.0, 1.0).into());
+pub fn render_cubes(
+    mut commands: Commands,
+    mut mesh_assets: ResMut<Assets<Mesh>>,
+    mut material_assets: ResMut<Assets<StandardMaterial>>
+) {
+    let mut mesh = mesh_assets.add(shape::Box::new(1.0, 1.0, 1.0).into());
+    let mut material = material_assets.add(StandardMaterial {
+        base_color: Color::rgb(0.2, 0.8, 0.2),
+        ..Default::default()
+    });
     for x in -10..10 {
         for z in -10..10 {
             for y in -10..10 {
@@ -13,6 +24,7 @@ pub fn render_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>
                         y as f32 * 2.,
                         z as f32 * 2.,
                     )),
+                    material: material.clone(),
                     ..Default::default()
                 });
             }
